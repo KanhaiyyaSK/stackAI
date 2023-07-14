1172626175b3a9daebd1286f6acf6ccd8d8c9b5f
KanhaiyyaSK <97464146+KanhaiyyaSK@users.noreply.github.com>
2023-07-14 22:19:53 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import userRoutes from "./routes/users.js";
+import questionRoutes from "./routes/Questions.js";
+import answerRoutes from "./routes/Answers.js";
+import chatRoutes from "./routes/ChatAi.js";
+
+const app = express();
+dotenv.config();
+cors();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(morgan("dev"));
+
+app.use("/chat/", chatRoutes);
+app.use("/user", userRoutes);
+app.use("/questions", questionRoutes);
+app.use("/answer", answerRoutes);
+
+app.get("/", (req, res) => {
+  res.send("This is a stack overflow clone API");
+});
+
+const PORT = process.env.PORT || 5000;
+
+const DATABASE_URL = process.env.CONNECTION_URL;
+
+mongoose
+  .connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err.message));
